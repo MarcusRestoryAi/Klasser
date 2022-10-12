0a63b0b98eac2a429c5621d51197b43f83f2da46
Marcus <marcus.henriksson@restory.ai>
2022-10-12 10:03:04 +0200

    Add project files.

---
+++ Klasser/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasser
+{
+    internal class Person
+    {
+        //Attribut
+        public string name;
+        public int age;
+
+        //Konstruktor
+        public Person(string name, int age)
+        {
+            //Tilldela värden till attributen
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
